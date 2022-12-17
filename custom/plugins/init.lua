2e00536b815c5c8380431c1a9ae9208dc3e06808
Vasilis Christopoulos <vaschristopoulos@gmail.com>
2022-12-18 00:10:47 +0200

    added lsp progress status

--- custom/plugins/init.lua
+++ custom/plugins/init.lua
@@ -1,85 +1,96 @@
-local overrides = require "custom.plugins.configs.overrides"
+local overrides = require("custom.plugins.configs.overrides")
 
 return {
 
-  ----------------------------------------- default plugins ------------------------------------------
+	----------------------------------------- default plugins ------------------------------------------
 
-  ["folke/which-key.nvim"] = {
-    disable = false,
-  },
+	["folke/which-key.nvim"] = {
+		disable = false,
+	},
 
-  ["goolord/alpha-nvim"] = {
-    disable = false,
-    cmd = "Alpha",
-  },
+	["goolord/alpha-nvim"] = {
+		disable = false,
+		cmd = "Alpha",
+	},
 
-  ["neovim/nvim-lspconfig"] = {
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.plugins.configs.lspconfig"
-    end,
-  },
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.plugins.configs.lspconfig")
+		end,
+	},
 
-  -- override default configs
-  ["kyazdani42/nvim-tree.lua"] = { override_options = overrides.nvimtree },
-  ["nvim-treesitter/nvim-treesitter"] = { override_options = overrides.treesitter },
-  ["lukas-reineke/indent-blankline.nvim"] = { override_options = overrides.blankline },
-  ["williamboman/mason.nvim"] = { override_options = overrides.mason },
+	-- override default configs
+	["kyazdani42/nvim-tree.lua"] = { override_options = overrides.nvimtree },
+	["nvim-treesitter/nvim-treesitter"] = { override_options = overrides.treesitter },
+	["lukas-reineke/indent-blankline.nvim"] = { override_options = overrides.blankline },
+	["williamboman/mason.nvim"] = { override_options = overrides.mason },
 
-  -- disable defautls
-  ["NvChad/nvterm"] = { disable = true },
+	-- disable defautls
+	["NvChad/nvterm"] = { disable = true },
 
-  --------------------------------------------- custom plugins ----------------------------------------------
+	--------------------------------------------- custom plugins ----------------------------------------------
 
-  -- autoclose tags in html, jsx only
-  ["windwp/nvim-ts-autotag"] = {
-    ft = { "html", "javascriptreact" },
-    after = "nvim-treesitter",
-    config = function()
-      local present, autotag = pcall(require, "nvim-ts-autotag")
+	-- autoclose tags in html, jsx only
+	["windwp/nvim-ts-autotag"] = {
+		ft = { "html", "javascriptreact" },
+		after = "nvim-treesitter",
+		config = function()
+			local present, autotag = pcall(require, "nvim-ts-autotag")
 
-      if present then
-        autotag.setup()
-      end
-    end,
-  },
+			if present then
+				autotag.setup()
+			end
+		end,
+	},
 
-  -- format & linting
-  ["jose-elias-alvarez/null-ls.nvim"] = {
-    after = "nvim-lspconfig",
-    config = function()
-      require "custom.plugins.configs.null-ls"
-    end,
-  },
+	-- format & linting
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.configs.null-ls")
+		end,
+	},
 
-  -- get highlight group under cursor
-  ["nvim-treesitter/playground"] = {
-    cmd = "TSCaptureUnderCursor",
-    config = function()
-      require("nvim-treesitter.configs").setup()
-    end,
-  },
+	-- get highlight group under cursor
+	["nvim-treesitter/playground"] = {
+		cmd = "TSCaptureUnderCursor",
+		config = function()
+			require("nvim-treesitter.configs").setup()
+		end,
+	},
 
-  -- dim inactive windows
-  ["andreadev-it/shade.nvim"] = {
-    opt = true,
-    config = function()
-      require "custom.plugins.configs.shade"
-    end,
-  },
+	-- dim inactive windows
+	["andreadev-it/shade.nvim"] = {
+		opt = true,
+		config = function()
+			require("custom.plugins.configs.shade")
+		end,
+	},
 
-  ["RishabhRD/lspactions"] = {
-    requires = { "nvim-lua/popup.nvim" },
-  },
+	["RishabhRD/lspactions"] = {
+		requires = { "nvim-lua/popup.nvim" },
+	},
 
-  ["nvim-telescope/telescope-symbols.nvim"] = {},
+	["nvim-telescope/telescope-symbols.nvim"] = {},
 
-  ["kdheepak/lazygit.nvim"] = {},
+	["kdheepak/lazygit.nvim"] = {},
 
-  ["akinsho/toggleterm.nvim"] = {
-    tag = "*",
-    config = function()
-      require "custom.plugins.configs.toggleterm"
-    end,
-  },
+	["akinsho/toggleterm.nvim"] = {
+		tag = "*",
+		config = function()
+			require("custom.plugins.configs.toggleterm")
+		end,
+	},
+	["j-hui/fidget.nvim"] = {
+		config = function()
+			local status_ok, fidget = pcall(require, "fidget")
+
+			if not status_ok then
+				return
+			end
+
+			fidget.setup()
+		end,
+	},
 }
