b12d6dfc092fd27e66d360cdb35d2a32c71fd40f
Vasilis Christopoulos <vaschristopoulos@gmail.com>
2022-12-17 17:22:36 +0200

    Added some keymappings and added lazygit

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -5,17 +5,28 @@ end
 local M = {}
 
 M.general = {
-  n = {
-    ["<C-/>"] = { "gcc", "toggle comment" },
-    ["<a-cr>"] = { "<cmd>lua require'lspactions'.code_action()<cr>", "Code Action", opts = { silent = true } },
-  },
+	n = {
+		["<C-/>"] = { "gcc", "toggle comment" },
+		["<a-cr>"] = { "<cmd>lua require'lspactions'.code_action()<cr>", "Code Action", opts = { silent = true } },
+	},
 
-  t = { ["<C-x>"] = { termcodes("<C-\\><C-N>"), "escape terminal mode" } },
+	t = { ["<C-x>"] = { termcodes("<C-\\><C-N>"), "escape terminal mode" } },
 
-  v = {
+	v = {
 		["<Up>"] = { 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', opts = { expr = true } },
 		["<Down>"] = { 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', opts = { expr = true } },
 		["<C-/>"] = { "gb", "toggle comment" },
+
+		["J"] = { ":m '>+1<CR>gv=gv", "Move lines below" },
+		["K"] = { ":m '<-2<CR>gv=gv", "Move lines above" },
+	},
+}
+
+M.lazygit = {
+	plugin = true,
+
+	n = {
+		["<leader>gg"] = { ":LazyGit<cr>", "Lazygit" },
 	},
 }
 
