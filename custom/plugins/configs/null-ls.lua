217591dbb09749214a9b6a1a95f1840f1f6ecaa9
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2022-12-28 12:40:58 +0200

    added actions to null-ls for typescript

--- custom/plugins/configs/null-ls.lua
+++ custom/plugins/configs/null-ls.lua
@@ -1,7 +1,7 @@
 local present, null_ls = pcall(require, "null-ls")
 
 if not present then
-  return
+	return
 end
 
 local diagnostics = null_ls.builtins.diagnostics
@@ -9,28 +9,29 @@ local formatting = null_ls.builtins.formatting
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local sources = {
-  formatting.phpcsfixer,
-  formatting.prettier,
-  formatting.stylua,
-  formatting.phpcbf,
-  diagnostics.shellcheck.with { diagnostics_format = "#{m} [#{c}]" },
+	formatting.phpcsfixer,
+	formatting.prettier,
+	formatting.stylua,
+	formatting.phpcbf,
+	diagnostics.shellcheck.with({ diagnostics_format = "#{m} [#{c}]" }),
+	require("typescript.extensions.null-ls.code-actions"),
 }
 
 local on_attach = function(client, bufnr)
-  if client.supports_method "textDocument/formatting" then
-    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format { bufnr = bufnr }
-      end,
-    })
-  end
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				vim.lsp.buf.format({ bufnr = bufnr })
+			end,
+		})
+	end
 end
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-  on_attach = on_attach,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = on_attach,
+})
