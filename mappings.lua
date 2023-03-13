f4e17f7ccea17315df3e5e0eeb46ad3b5342c803
Vasilis Chrtistopoulos <vaschristopoulos@gmail.com>
2023-03-13 23:47:00 +0200

    feat: added lazygit

--- mappings.lua
+++ mappings.lua
@@ -2,9 +2,10 @@
 local M = {}
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-  },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["<leader>gg"] = { "<CMD>LazyGit<CR>", "open lazygit", opts = { nowait = true, silent = true } },
+	},
 }
 
 -- more keybinds!
